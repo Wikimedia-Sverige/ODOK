d98e84bb64112b799ba8882122026160aae84d8b
lokal-profil <lokal.profil@gmail.com>
2013-07-29 20:41:26 +0200

    Translated comments to english (rather than the previous mixture)

--- database/odok-setup.sql
+++ database/odok-setup.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS `source_table`;
 DROP TABLE IF EXISTS `artist_table`;
 
 CREATE TABLE  `muni_table` (
-  `id`          smallint(4)     NOT NULL,                   #Kommunkod
-  `name`        varchar(255)    NOT NULL,                   #Kommunnamn
+  `id`          smallint(4)     NOT NULL,                   #Municipal code
+  `name`        varchar(255)    NOT NULL,                   #Name of municipality
   `wiki`        varchar(255)    NOT NULL DEFAULT '',        #article name on wikidata
   PRIMARY KEY   `id` (`id`),
   INDEX         `name` (`name`)
@@ -19,8 +19,8 @@ INSERT INTO muni_table (id, name) VALUES ("0000", "unknown");
 LOAD DATA INFILE "/tmp/muni.dat" INTO TABLE muni_table CHARACTER SET UTF8 FIELDS TERMINATED BY '|' IGNORE 1 LINES (id, name, @dummy, wiki);
 
 CREATE TABLE  `county_table` (
-  `id`          varchar(2)      NOT NULL,                   #Länskod
-  `name`        varchar(255)    NOT NULL,                   #Länsnamn
+  `id`          varchar(2)      NOT NULL,                   #County code
+  `name`        varchar(255)    NOT NULL,                   #Name of county
   `wiki`        varchar(255)    NOT NULL DEFAULT '',        #article name on wikidata
   PRIMARY KEY   `id` (`id`),
   INDEX         `name` (`name`)
@@ -29,30 +29,30 @@ INSERT INTO county_table (id, name) VALUES ("00", "unknown");
 LOAD DATA INFILE "/tmp/county.dat" INTO TABLE county_table CHARACTER SET UTF8 FIELDS TERMINATED BY '|' IGNORE 1 LINES (id, name, @dummy, wiki);
 
 CREATE TABLE  `source_table` (
-  `id`          varchar(25)     NOT NULL,                   # Unikt id för källan
-  `name`        varchar(255)    NOT NULL,                   # Källans hela namn
+  `id`          varchar(25)     NOT NULL,                   # Unique id for the source
+  `name`        varchar(255)    NOT NULL,                   # Full name of the source
   `wiki`        varchar(255)    NOT NULL DEFAULT '',        # Article name on wikidata
-  `real_id`     bit(1)          NOT NULL DEFAULT 1,         # Försåg de oss med egna id? t/f = 1/0
-  `url`         varchar(255)    NOT NULL DEFAULT '',        # Källans webbsida
-  `changed`     timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, # Senast ändrad
-  `cmt`         text            NOT NULL DEFAULT '',        # Kommentar om datakällan (t.ex. bara utomhusföremål, bara ägda av kommunene etc.)
-  #`updates`    bit(1)          NOT NULL DEFAULT 0,         # Vill de ha uppdaterad data? t/f = 1/0
+  `real_id`     bit(1)          NOT NULL DEFAULT 1,         # Did they provide their own ids? t/f = 1/0
+  `url`         varchar(255)    NOT NULL DEFAULT '',        # Source webpage
+  `changed`     timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, # Last changed
+  `cmt`         text            NOT NULL DEFAULT '',        # Comment for source (e.g. only outdoor objects, only objects owned by the municipality etc.)
+  #`updates`    bit(1)          NOT NULL DEFAULT 0,         # Did they request updated data? t/f = 1/0
   PRIMARY KEY   `id` (`id`),
   INDEX         `name` (`name`)
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `artist_table` (
-  `id`          int             NOT NULL AUTO_INCREMENT,    # Unikt id för artisten
-  `first_name`  varchar(255)    NOT NULL,                   # Förnamn (eller hela namnet?)
-  `last_name`   varchar(255)    NOT NULL DEFAULT '',        # Efternamn
+  `id`          int             NOT NULL AUTO_INCREMENT,    # Unique id for the artist
+  `first_name`  varchar(255)    NOT NULL,                   # First name (or whole name if no last name)
+  `last_name`   varchar(255)    NOT NULL DEFAULT '',        # Last name
   `wiki`        varchar(255)    NOT NULL DEFAULT '',        # Article name on wikidata
-  `changed`     timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, # Senast ändrad
+  `changed`     timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, # Last changed
   `birth_date`  date            DEFAULT NULL,               # Birth date (full iso date)
   `death_date`  date            DEFAULT NULL,               # Death date (full iso date)
   `birth_year`  year            DEFAULT NULL,               # Birth year (automatically filled in if birth_date is entered)
   `death_year`  year            DEFAULT NULL,               # Death year (automatically filled in if death_date is entered)
   `creator`     varchar(255)    NOT NULL DEFAULT '',        # Creator template on commons
-  `cmt`         text            NOT NULL DEFAULT '',        # Kommentar om artisten (t.ex. källa för informationen etc.)
+  `cmt`         text            NOT NULL DEFAULT '',        # Comment for artisten (e.g. source of information etc.)
   PRIMARY KEY   `id` (`id`),
   INDEX         `name` (`first_name`, `last_name`),
   INDEX         `death` (`death_year`),
@@ -62,34 +62,34 @@ CREATE TABLE  `artist_table` (
 
 #avoid using NULL as default since this makes trigger comparisons bulky
 CREATE TABLE  `main_table` (
-  `id`          varchar(25)     NOT NULL,                   # Unikt id
-  `title`       varchar(255)    NOT NULL DEFAULT '',        # Namn
-  `artist`      varchar(255)    NOT NULL DEFAULT '',        # Konstnär
-  `descr`       text            NOT NULL DEFAULT '',        # Beskrivning (fritext) (text?)
-  `year`        smallint(4)     NOT NULL DEFAULT '0000',    # årtal, 0000 som okänd
-  `year_cmt`    text            NOT NULL DEFAULT '',        # Kommentar om år är okänt
-  `type`        varchar(255)    NOT NULL DEFAULT '',        # typ av konstverk (table?)
-  `material`    varchar(255)    NOT NULL DEFAULT '',        # material konstverket är gjort av (fritext)
-  `inside`      bit(1)          NOT NULL,                   # inomhus eller utomhus, för FoP
-  `address`     varchar(255)    NOT NULL DEFAULT '',        # adress för konstverk (relevant?)
-  `county`      varchar(2)      NOT NULL REFERENCES county_table(id),   # länskod, 00 som okänd
-  `muni`        smallint(4)     NOT NULL REFERENCES muni_table(id),     # kommunkod, 0000 som okänd
-  `district`    varchar(255)    NOT NULL DEFAULT '',        # stadsdel
+  `id`          varchar(25)     NOT NULL,                   # Unique id
+  `title`       varchar(255)    NOT NULL DEFAULT '',        # Namn of artwork
+  `artist`      varchar(255)    NOT NULL DEFAULT '',        # Artist
+  `descr`       text            NOT NULL DEFAULT '',        # Description (free text)
+  `year`        smallint(4)     NOT NULL DEFAULT '0000',    # year, 0000 if unknown
+  `year_cmt`    text            NOT NULL DEFAULT '',        # Comment about year (e.g. why unknown or to specify completionyear or erectionyear etc.)
+  `type`        varchar(255)    NOT NULL DEFAULT '',        # type of artwork (table?)
+  `material`    varchar(255)    NOT NULL DEFAULT '',        # material the artwork is made of (freetext) (Could make it a separate table)
+  `inside`      bit(1)          NOT NULL,                   # indoors or outdoors, for FoP
+  `address`     varchar(255)    NOT NULL DEFAULT '',        # address/placement of object
+  `county`      varchar(2)      NOT NULL REFERENCES county_table(id),   # coutny code, 00 if unknown
+  `muni`        smallint(4)     NOT NULL REFERENCES muni_table(id),     # municipal code, 0000 if unknown
+  `district`    varchar(255)    NOT NULL DEFAULT '',        # district of city or town in a rural municipality
   `lat`         double          DEFAULT NULL,               # WGS84 latitude (decimal format)
   `lon`         double          DEFAULT NULL,               # WGS84 longitud (decimal format)
-  `image`       varchar(255)    NOT NULL DEFAULT '',        # Bildnamn hos Commons
-  `source`      varchar(25)     NOT NULL REFERENCES source_table(id),   # Källa för info (organisation)
-  `ugc`         bit(1)          NOT NULL DEFAULT 0,         # Flagga för om det är originalinfo t/f =1/0
-  `changed`     timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, # Senast ändrad
-  `created`     timestamp,                                  # När objektet lades in i databasen (får rätt värde genom created_trigger)
-  `wiki_article` varchar(255)   NOT NULL DEFAULT '',        # WikiData id för artikel om konstverket
-  `commons_cat`  varchar(255)   NOT NULL DEFAULT '',        # Objektspecifik kategori på Commons
-  `official_url` varchar(255)   NOT NULL DEFAULT '',        # Officiell sida om objektet (på källans webplats)
-  `same_as`     varchar(25)     DEFAULT NULL REFERENCES main_table(id), # Om samma objekt har två id (t.ex. kommit från kommun och län)
-  `free`        enum('','pd','cc','unfree') NOT NULL DEFAULT '',        # Om objektet är upphovsrättsligt fritt. '' då okänd.
-  #`free_cmt`   text            NOT NULL DEFAULT '',        # Motivation för free status t.ex. artist dödsår ifall inget artist_id
-  `owner`       varchar(255)    NOT NULL DEFAULT '',        # Ägare av, eller ansvarig för, objektet
-  `cmt`         text            NOT NULL DEFAULT '',        # Kommentarer så som tillfälligt, stod tidigare nån annnan stans etc.
+  `image`       varchar(255)    NOT NULL DEFAULT '',        # Image name on Wikimedia Commons
+  `source`      varchar(25)     NOT NULL REFERENCES source_table(id),   # Source of information (organisation)
+  `ugc`         bit(1)          NOT NULL DEFAULT 0,         # Flag indicating whether information has been modified t/f =1/0
+  `changed`     timestamp       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, # last changed
+  `created`     timestamp,                                  # timstamp for entry into database (gets its value through the created_trigger)
+  `wiki_article` varchar(255)   NOT NULL DEFAULT '',        # Wikidata id for the article about the artwork
+  `commons_cat`  varchar(255)   NOT NULL DEFAULT '',        # Object specific category on Wikimedia Commons
+  `official_url` varchar(255)   NOT NULL DEFAULT '',        # Official page about the object (on source web page)
+  `same_as`     varchar(25)     DEFAULT NULL REFERENCES main_table(id), # If this is a duplicate of an exisitnig object (e.g. object provided by two sources)
+  `free`        enum('','pd','cc','unfree') NOT NULL DEFAULT '',        # If the object is free (copyrightwise). '' if unknown
+  #`free_cmt`   text            NOT NULL DEFAULT '',        # Motivation for free status (e.g. artist deathyear if no linked artist)
+  `owner`       varchar(255)    NOT NULL DEFAULT '',        # Owner of, or carer for, the object
+  `cmt`         text            NOT NULL DEFAULT '',        # Comments such as temporary placements, previously placed somewhere else, damaged etc.
   PRIMARY KEY   `id` (`id`),
   INDEX         `title` (`title`),
   INDEX         `county` (`county`),
@@ -148,8 +148,8 @@ CREATE TABLE  `audit_table` (
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE  `artist_links` (
-  `object`      varchar(25)     NOT NULL REFERENCES main_table(id),     #Objekt_id
-  `artist`      int             NOT NULL REFERENCES artist_table(id),   #Konstnärs_id
+  `object`      varchar(25)     NOT NULL REFERENCES main_table(id),     #Object id
+  `artist`      int             NOT NULL REFERENCES artist_table(id),   #Artist id
   PRIMARY KEY   `object-artist` (`object`, `artist`),
   UNIQUE INDEX  `artist-object` (`artist`, `object`),
   #Because mysql does not support inline referenes
