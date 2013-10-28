ab2a198e681a7a052110f34616f695fb376fac98
lokal-profil <lokal.profil@gmail.com>
2013-10-28 14:07:58 +0100

    Removing methods depricated in favour of WikiApi and odok modules

--- tools/common.py
+++ tools/common.py
@@ -5,8 +5,6 @@
 import codecs
 import os
 import operator
-import urllib, urllib2
-from json import loads
 
 def openFile(filename):
     '''opens a given file (utf-8) and returns the lines'''
@@ -53,94 +51,6 @@ def extractNameParts(name):
         fName = name[:-len(lName)].strip()
         return u'%s;%s' %(fName,lName)
     return name
-
-def getWikidata(articles, dDict=None, verbose=False, language='sv', family='wikipedia', old=True):
-    '''
-    #largely replaced by WikiApi.getPageInfo() - only the simplified formating is  not included
-    returns the wikidata enitity id of an article/list of articles or a comment if none can be found
-    @input:  article/list of articles
-             dDict: dictionary to which to add the results as {article:wikidata} [None]
-             verbose: verbose output from wdFormat [False]
-             language/family: language and family for api [sv/wikipedia]
-             old: option for backwards compatibility wherby only wikidata_entity/None is returned (for a list of articles) [True]
-    @output: single article suplied: (wikidata entity, comment) formated through wdFormat()
-             list of articles supplied: dictonary as {article:wikidata}
-    TO DO: Once "old" can be removed pDict may be bypassed in favour of dDict
-    '''
-    if not dDict:
-        dDict ={}
-    single=False
-    if not isinstance(articles,list):
-        if isinstance(articles,str):
-            articles = [articles,]
-            single=True
-        else:
-            print '"getWikidata()" requires a list of articles or a single article as the first parameter.'
-            return None
-    pDict ={}
-    getPageInfo(articles, pDict, language=language, family=family)
-    if single:
-        return wdFormat(articles[0], pDict[articles[0]], verbose=verbose)
-    elif old:
-        for k, v in pDict.iteritems():
-            pInfo = wdFormat(k, v, verbose=verbose)
-            if pInfo[0]:
-                dDict[k] = pInfo[0]
-            else:
-                dDict[k] = None
-        return dDict #in case one was not supplied
-    else:
-        for k, v in pDict.iteritems():
-            dDict[k] = v
-        return dDict #in case one was not supplied
-
-def getManyWikidata(articles, dDict, verbose=False, language='sv', family='wikipedia'):
-    '''
-    DEPRECATED, use new getWikidata() instead. Make sure old call is not followed by a call to getPageInfo()
-    '''
-    print 'getManyWikidata() is DEPRECATED use new getWikidata() instead'
-    return getWikidata(article, dDict=dDict, verbose=verbose, language=language, family=family)
-
-def getManyArticles(wikidata, dDict, verbose=False):
-    '''
-    DEPRECATED to WikiDataApi.getArticles()
-    returns the articles of a list of wikidata enitity ids
-    '''
-    print 'getManyArticles() is DEPRECATED use new WikiDataApi.getArticles() instead'
-    if not isinstance(wikidata,list):
-        print '"getManyArticles" requiresa list of articles. for individual wikidata entities use "getArticles" instead'
-        return None
-    #do an upper limit check (max 50 titles per request allowed)
-    if len(wikidata) > 50:
-        i=0
-        while True:
-            getManyArticles(wikidata[i:i+50], dDict, verbose=verbose)
-            i=i+50
-            if i+50 > len(wikidata):
-                getManyArticles(wikidata[i:], dDict, verbose=verbose)
-                break
-    elif len(wikidata) > 0:
-        wikiurl = u'https://www.wikidata.org'
-        apiurl = '%s/w/api.php' %wikiurl
-        urlbase = '%s?action=wbgetentities&format=json&props=sitelinks&ids=' %apiurl
-        url = urlbase+urllib.quote('|'.join(wikidata).encode('utf-8'))
-        if verbose: print url
-        req = urllib2.urlopen(url)
-        j = loads(req.read())
-        req.close()
-        if (j['success'] == 1) or (not 'warnings' in j.keys()) or (not len(j['entities'])==1):
-            for k, v in j['entities'].iteritems():
-                if 'svwiki' in v['sitelinks'].keys():
-                    title = v['sitelinks']['svwiki']['title']
-                    dDict[k] = title
-                    if verbose: print u'%s: Found the title at %s' %(k,title)
-                else:
-                    dDict[k] = None
-                    if verbose: print '%s: no entry' %k
-        else:
-            error = 'success: %s, warnings: %s, entries: %d' %(j['success'], 'warnings' in j.keys(), len(j['entities']))
-            if verbose: print error
-            return (None, error)
 
 def findUnit(contents, start, end, brackets=None):
     '''
@@ -267,7 +177,7 @@ def extractLink(text, kill_tags=False):
 
 def extractAllLinks(text, kill_tags=False):
     '''
-    Given wikitiext this checks for any wikilinks
+    Given wikitext this checks for any wikilinks
     @output: (plain_text, list of link)
     '''
     wikilinks=[]
@@ -302,146 +212,4 @@ def latLonFromCoord(coord):
     lon = lon_sign*lon
     return (lat,lon)
 
-def getPage(page, verbose=False, language='sv', family='wikipedia'):
-    '''
-    DEPRECATED to WikiApi.getPage()
-    Given an article this returns the contents of (the latest revision of) the page
-    @ input: pagetitle to look at
-    @ output: contents of page
-    '''
-    print 'getPage() is DEPRECATED use new WikiDataApi.getPage() instead'
-    apiurl = u'https://%s.%s.org/w/api.php' %(language, family)
-    urlbase = '%s?action=query&prop=revisions&format=json&rvprop=content&rvlimit=1&titles=' %apiurl
-    url = urlbase+urllib.quote(page.encode('utf-8'))
-    if verbose: print url
-    req = urllib2.urlopen(url)
-    j = loads(req.read())
-    req.close()
-    pageid = j['query']['pages'].keys()[0]
-    if pageid == u'-1':
-        if verbose: print 'no entry for "%s"' %page
-        return None
-    else:
-        content = j['query']['pages'][pageid]['revisions'][0]['*']
-        return content
-
-def getPageTimestamp(page, verbose=False, language='sv', family='wikipedia'):
-    '''
-    DEPRECATED to WikiApi.getTimestamp()
-    Given an article this returns the timestamp of (the latest revision of) the page
-    @ input: pagetitle to look at
-    @ output: timestamp in ISO 8601 format
-    '''
-    print 'getPageTimestamp() is DEPRECATED use new WikiDataApi.getTimestamp() instead'
-    apiurl = u'https://%s.%s.org/w/api.php' %(language, family)
-    urlbase = '%s?action=query&prop=revisions&format=json&rvprop=timestamp&rvlimit=1&titles=' %apiurl
-    url = urlbase+urllib.quote(page.encode('utf-8'))
-    if verbose: print url
-    req = urllib2.urlopen(url)
-    j = loads(req.read())
-    req.close()
-    pageid = j['query']['pages'].keys()[0]
-    if pageid == u'-1':
-        if verbose: print 'no entry for "%s"' %page
-        return None
-    else:
-        content = j['query']['pages'][pageid]['revisions'][0]['timestamp']
-        return content
-
-def getPageInfo(articles, dDict, verbose=False, language='sv', family='wikipedia'):
-    '''
-    DEPRECATED to WikiApi.getPageInfo()
-    Given a list of articles this tells us if the page is either of
-    * Non-existent (red link)
-    * A redirect (and returns real page)
-    * A disambiguity page
-    * Normal page
-    and whether the page corresponds to a wikidata entry (and returns the wikidata id)
-    @input: a list of pagenames or a single pagename
-    @output: fills in the supplied dict with the supplied pagenames and a dict of properties with possible parameters being: redirected, missing, disambiguation, wikidata
-    '''
-    print 'getPageInfo() is DEPRECATED use new WikiDataApi.getPageInfo() instead'
-    if not isinstance(articles,list):
-        if isinstance(articles,str):
-            articles = [articles,]
-        else:
-            print '"getPageInfo" requires a list of pagenames or a single pagename.'
-            return None
-    #do an upper limit check (max 50 titles per request allowed)
-    if len(articles) > 50:
-        i=0
-        while True:
-            getPageInfo(articles[i:i+50], dDict, verbose=verbose, language=language, family=family)
-            i=i+50
-            if i+50 > len(articles):
-                getPageInfo(articles[i:], dDict, verbose=verbose, language=language, family=family)
-                break
-    elif len(articles) > 0:
-        apiurl = u'https://%s.%s.org/w/api.php' %(language, family)
-        urlbase = u'%s?action=query&prop=pageprops&format=json&redirects=&titles=' %apiurl
-        url = urlbase+urllib.quote('|'.join(articles).encode('utf-8'))
-        if verbose: print url
-        req = urllib2.urlopen(url)
-        j = loads(req.read())
-        req.close()
-        
-        #start with redirects
-        rDict = {}
-        if 'redirects' in j['query'].keys():
-            redirects = j['query']['redirects'] #a list
-            for r in redirects:
-                rDict[r['to']] = r['from']
-        
-        #then pages
-        pages = j['query']['pages'] #a dict
-        for k, v in pages.iteritems():
-            page = {}
-            title = v['title']
-            #check if redirected
-            if title in rDict.keys():
-                page['redirect'] = title
-                title = rDict[title]
-            #missing
-            if 'missing' in v.keys():
-                page['missing'] = True
-            #properties
-            if 'pageprops' in v.keys():
-                u = v['pageprops']
-                if 'disambiguation' in u.keys():
-                    page['disambiguation'] = True
-                if 'wikibase_item' in u.keys():
-                    page['wikidata'] = u['wikibase_item'].upper()
-            dDict[title] = page.copy()
-
-def getAdvancedWikidata(article, verbose=False, language='sv', family='wikipedia'):
-    '''
-    Given a single article this checks for a corresponding wikidata entry
-    and formats the output
-    DEPRECATED, use new getWikidata() instead
-    '''
-    print 'getAdvancedWikidata() is DEPRECATED use new getWikidata() instead'
-    return getWikidata(article, verbose=verbose, language=language, family=family)
-
-def wdFormat(article, pInfo, verbose=False):
-    '''
-    Formats a single entry from the output of getPageInfo()
-    * If the page is not a disambiguation page and a wikidata entry is found then this 
-      is returned as the first parameter
-    * Else if the page is a DISAMBIGUATION, RED LINK or LACKS A WIKIDATA ENTITY this is returned as a second 
-      parameter (disambig, redLink, normal)
-    @ output (wikidata entity, comment)
-    '''
-    if 'disambiguation' in pInfo.keys():
-        if verbose: print u'%s: was a disambiguation page' % article
-        return (None, 'disambig')
-    elif 'wikidata' in pInfo.keys():
-        if verbose: print u'%s: has wikidata entry at %s' %(article,pInfo['wikidata'])
-        return (pInfo['wikidata'], None)
-    elif 'missing' in pInfo.keys():
-        if verbose: print u'%s: was a red link' % article
-        return (None, 'redLink')
-    else:
-        if verbose: print u'%s: needs a wikidata entry' % article
-        #add mechanism for creating wikidata entry
-        return (None, 'normal')
 #done
