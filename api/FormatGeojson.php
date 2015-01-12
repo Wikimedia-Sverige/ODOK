4374db3e5ada53e08dd7f4486b52a2d61e1cecfe
lokal-profil <lokal.profil@gmail.com>
2015-01-12 13:25:09 +0100

    Medium is the new Full

--- api/FormatGeojson.php
+++ api/FormatGeojson.php
@@ -20,96 +20,9 @@
             return ($geo);
         }
         
-        private function writeRowFull($row, $imgsize = 100){
-            $row = $row['hit'];
-            #Ignore rows without coords
-            if (!empty($row['lat']) and !empty($row['lon'])){
-                $feature = Array(
-                                 'type' => 'Feature',
-                                 'id' => $row['id'],
-                                 'geometry' => Array(
-                                                     'type' => 'Point',
-                                                     'coordinates' => Array((float)$row['lon'], (float)$row['lat'])
-                                                     )
-                                 );
-                $handled = Array('id','lat','lon');
-                
-                #Deal with properties
-                $prop=Array();
-                
-                #Spatial info
-                array_push($handled, 'inside','address','county','muni','district');
-                $spatial = Array(
-                                 'inside' => $row['inside'],
-                                 'address' => $row['address'],
-                                 'county' => 'SE-'. $row['county'],
-                                 'muni' =>  $row['muni'],
-                                 'district' => $row['district']
-                                 );
-                $prop['spatial'] = $spatial;
-                
-                #image
-                array_push($handled, 'image');
-                if ($row['image']) {
-                    $prop['image'] = Array(
-                            'filename' => $row['image'],
-                            'thumb' => ApiBase::getImageFromCommons($row['image'],$imgsize)
-                            );
-                }else {
-                    $prop['image'] = null;
-                }
-                
-                #descriptions
-                array_push($handled, 'descr', 'wiki_article', 'official_url');
-                $desc_text = Array(
-                                 'descr' => $row['descr'],
-                                 'wiki' => ApiBase::getArticleFromWikidata($row['wiki_article']),
-                                 'wikidata' => !empty($row['wiki_article']) ? $row['wiki_article'] : null,
-                                 'official' => !empty($row['official_url']) ? $row['official_url'] : null,
-                                 'ingress' => !empty($row['wiki_article']) ? ApiBase::getArticleIntro(ApiBase::getArticleFromWikidata($row['wiki_article'], $getUrl=false)) : null,
-                                 );
-                $prop['descriptions'] = $desc_text;
-                
-                #artists
-                array_push($handled, 'artist');
-                $artist = Array();
-                $artist_info = ApiBase::getArtistInfo($row['id']);
-                if (!empty($artist_info)){
-                   foreach ($artist_info as $ai){
-                         array_push($artist, Array(
-                                                   'wiki' => ApiBase::getArticleFromWikidata($ai['wiki']),
-                                                   'wikidata' => !empty($ai['wiki']) ? $ai['wiki'] : null,
-                                                   'name' => $ai['name'])
-                                                   );
-                   }
-                }elseif($row['artist']) {
-                   $artistsList = explode(';',$row['artist']);
-                   foreach ($artistsList as $a){
-                         array_push($artist, Array('name' => $a));
-                   }
-                }else {
-                    $artist = null;
-                }
-                $prop['artist'] = $artist;
-                
-                #any remaining
-                foreach($row as $key => $value){
-                    if (in_array($key, $handled)){
-                        continue;
-                    }else {
-                        $prop[$key] = $value;
-                    }
-                }
-                
-                #store and return
-                $feature['properties'] = $prop;
-                return($feature);
-            }
-        }
-
-        private function writeRowMedium($row, $imgsize = 100){
+        private function writeRowFull($row){
             # rows to skip
-            $skip = Array('created', 'changed', 'ugc', 'year_cmt', 'cmt', 'owner');
+            $skip = Array('created', 'changed', 'ugc', 'year_cmt', 'cmt', 'owner', 'official_url');
             
             $row = $row['hit'];
             #Ignore rows without coords
@@ -150,7 +63,7 @@
                 }
                 
                 #descriptions
-                array_push($handled, 'descr', 'wiki_article', 'official_url');
+                array_push($handled, 'descr', 'wiki_article');
                 $desc_text = Array(
                                  'descr' => $row['descr'],
                                  'wikidata' => !empty($row['wiki_article']) ? $row['wiki_article'] : null,
@@ -238,7 +151,7 @@
                 $features=Array();
                 foreach($results['body'] as $row){
                     if ($full){
-                        $f = self::writeRowMedium($row);
+                        $f = self::writeRowFull($row);
                     }else {
                         $f = self::writeRowBasic($row);
                     }
