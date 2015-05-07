421cc117082636b3bd947f8e659ea9a4c74c9aa9
lokal-profil <lokal.profil@gmail.com>
2015-05-07 17:01:51 +0200

    Set testing during setup of db connection
    Fix as a result of 4ed29dfa3ad25dba9864df1bc1b015127b9f549a
    
    Also some linting

--- tools/updateCopyright.py
+++ tools/updateCopyright.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
 '''
-Tool for updating the copyright status of an object based on the birth/death year of the artist(s)
+Tool for updating the copyright status of an object based on the
+birth/death year of the artist(s)
 ToDO:
     tag free objects with multiple artists
     update unfree object once it becomes free (relies on multi-artistfix)
 '''
 import odok as odokConnect
 import dconfig as config
-# import codecs
 
-YEARS_AFTER_BIRTH = 150  # Number of years since birth before considering the work to be free (if year of death is unknown)
-YEARS_AFTER_DEATH = 70  # Number of years since death before considering the work to be free
+# Number of years since birth before considering the work to be free (if year of death is unknown)
+YEARS_AFTER_BIRTH = 150
+# Number of years since death before considering the work to be free
+YEARS_AFTER_DEATH = 70
+
 
 def tagUnfree(dbWriteSQL, testing):
     '''
-    identifies unfree objects based on the birth and/or death year of the artist
-    also tags any object with multiple artists
+    identifies unfree objects based on the birth and/or death year of
+    the artist and also tags any object with multiple artists
     '''
-    if testing: query=u"""##would change free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
-    else: query = u"""UPDATE `main_table` SET free=%s """
+    if testing:
+        query = u"""##would change free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
+    else:
+        query = u"""UPDATE `main_table` SET free=%s """
     query += u"""WHERE `free` = '' AND `id` IN
     (SELECT `object` FROM `artist_links` WHERE `artist` IN
         (SELECT `id` FROM `artist_table` WHERE
@@ -39,11 +44,12 @@ def tagUnfree(dbWriteSQL, testing):
             )
         )
     );"""
-    affected_count, results = dbWriteSQL.query(query, (u'unfree', YEARS_AFTER_DEATH, YEARS_AFTER_DEATH), expectReply=True, testing=testing)
+    affected_count, results = dbWriteSQL.query(query, (u'unfree', YEARS_AFTER_DEATH, YEARS_AFTER_DEATH), expectReply=True)
     # do something with the reply
-    print u'-----------\n unfree (%d)\nid | free | title | artist' %affected_count
+    print u'-----------\n unfree (%d)\nid | free | title | artist' % affected_count
     for row in results:
         print ' | '.join(row)
+
 
 def tagFree(dbWriteSQL, testing):
     '''
@@ -51,8 +57,10 @@ def tagFree(dbWriteSQL, testing):
     skips any object with multiple artists
     '''
 
-    if testing: query=u"""##would set free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
-    else: query = u"""UPDATE `main_table` SET free=%s """
+    if testing:
+        query = u"""##would set free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
+    else:
+        query = u"""UPDATE `main_table` SET free=%s """
     query += u"""WHERE `free` = '' AND `id` IN
     (SELECT a.`object` FROM `artist_links` a
         JOIN (
@@ -74,11 +82,12 @@ def tagFree(dbWriteSQL, testing):
             )
         )
     );"""
-    affected_count, results = dbWriteSQL.query(query, (u'pd', YEARS_AFTER_BIRTH, YEARS_AFTER_DEATH), expectReply=True, testing=testing)
+    affected_count, results = dbWriteSQL.query(query, (u'pd', YEARS_AFTER_BIRTH, YEARS_AFTER_DEATH), expectReply=True)
     # do something with the reply
-    print u'-----------\n free (%d)\nid | free | title | artist' %affected_count
+    print u'-----------\n free (%d)\nid | free | title | artist' % affected_count
     for row in results:
         print ' | '.join(row)
+
 
 def tagOldUnknown(dbWriteSQL, testing):
     '''
@@ -87,32 +96,37 @@ def tagOldUnknown(dbWriteSQL, testing):
     * free:    year + YEARS_AFTER_BIRTH < NOW
     '''
     # Unfree if year + YEARS_AFTER_DEATH > NOW
-    if testing: query=u"""##would set free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
-    else: query = u"""UPDATE `main_table` SET free=%s """
+    if testing:
+        query = u"""##would set free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
+    else:
+        query = u"""UPDATE `main_table` SET free=%s """
     query += u"""WHERE
     `free` = '' AND
     `artist` = '' AND NOT
     `year` = 0 AND
     `year` + %r > YEAR(CURRENT_TIMESTAMP);"""
-    affected_count, results = dbWriteSQL.query(query, (u'unfree', YEARS_AFTER_DEATH), expectReply=True, testing=testing)
+    affected_count, results = dbWriteSQL.query(query, (u'unfree', YEARS_AFTER_DEATH), expectReply=True)
     # do something with the reply
-    print u'-----------\n unfree no artist (%d)\nid | free | title | artist' %affected_count
+    print u'-----------\n unfree no artist (%d)\nid | free | title | artist' % affected_count
     for row in results:
         print ' | '.join(row)
 
     # Free if year + YEARS_AFTER_BIRTH < NOW
-    if testing: query=u"""##would set free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
-    else: query = u"""UPDATE `main_table` SET free=%s """
+    if testing:
+        query = u"""##would set free=%s\nSELECT `id`, `free`, `title`, `artist` FROM `main_table` """
+    else:
+        query = u"""UPDATE `main_table` SET free=%s """
     query += u"""WHERE
     `free` = '' AND
     `artist` = '' AND NOT
     `year` = 0 AND
     `year` + %r < YEAR(CURRENT_TIMESTAMP);"""
-    affected_count, results = dbWriteSQL.query(query, (u'pd', YEARS_AFTER_BIRTH), expectReply=True, testing=testing)
+    affected_count, results = dbWriteSQL.query(query, (u'pd', YEARS_AFTER_BIRTH), expectReply=True)
     # do something with the reply
-    print u'-----------\n free no artist (%d)\nid | free | title | artist' %affected_count
+    print u'-----------\n free no artist (%d)\nid | free | title | artist' % affected_count
     for row in results:
         print ' | '.join(row)
+
 
 def showMultipleArtists(dbWriteSQL, testing):
     '''
@@ -124,14 +138,15 @@ def showMultipleArtists(dbWriteSQL, testing):
             SELECT `object`, COUNT(*) c FROM `artist_links` GROUP BY `object` HAVING c > 1
         ) b ON a.`object`=b.`object`
     );"""
-    affected_count, results = dbWriteSQL.query(query, None, expectReply=True, testing=testing)
+    affected_count, results = dbWriteSQL.query(query, None, expectReply=True)
     # do something with the reply
-    print u'-----------\n with multiple artists and no status (%d)\nid | free | title | artist' %affected_count
+    print u'-----------\n with multiple artists and no status (%d)\nid | free | title | artist' % affected_count
     for row in results:
         print ' | '.join(row)
 
+
 def run(testing=False):
-    dbWriteSQL = odokConnect.OdokWriter.setUp(host=config.db_server, db=config.db, user=config.db_edit, passwd=config.db_edit_password)
+    dbWriteSQL = odokConnect.OdokWriter.setUp(host=config.db_server, db=config.db, user=config.db_edit, passwd=config.db_edit_password, testing=testing)
     tagUnfree(dbWriteSQL, testing=testing)
     tagFree(dbWriteSQL, testing=testing)
     tagOldUnknown(dbWriteSQL, testing=testing)
