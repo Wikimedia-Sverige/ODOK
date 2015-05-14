4db39586ad16eeba0fecfdfe4d9ba36091296c0f
freenuxx <euomu@inventati.org>
2015-05-14 20:04:45 +0200

    01-[css,js] Popup look and thumbnail
    In my.css: avoids affecting the line after the GitHub link in the
    information window (the three vertical bars) by moving margin-bottom:0px
    from ul to .leaflet-popup-content ul; changes back the thumbnail max-
    height from 160px to 100px.
    In map.js: by reffering to only the displays with a width smaller than
    380(instead of 800 as before), it reverses the behavior to the previous
    one, avoiding on a short but wide display (mobile on the side) the slide
    to move the window off the point where the pop-up is bound, thereby
    forcing it to close; changes back the thumbnail max-width from 160px to
    100px; fixes indentation.

--- site/map.js
+++ site/map.js
@@ -145,10 +145,10 @@ $(document).ready(function() {
             .openOn(map);
     }
     map.on('contextmenu', onMapClick);
-    
+
     // center popup AND marker to the map + 23% of clientHeight
     // to position it under the leaflet-control-locate icon on small displays
-    if ($(window).width() <= 800){
+    if ($(window).width() <= 380){
         map.on('popupopen', function(e) {
         var px = map.project(e.popup._latlng); // find the pixel location on the map where the popup anchor is
         px.y -= e.popup._container.clientHeight/2+e.popup._container.clientHeight*0.23; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
@@ -162,7 +162,7 @@ function makePopup(feature) {
     // Based on https://stackoverflow.com/questions/10889954
     var properties = feature.properties;
     var desc = "";
-    
+
     // code originally from updateFeatures.py
     // list-link
     if (properties.descriptions.list) {
@@ -181,7 +181,7 @@ function makePopup(feature) {
             showImage = properties.image.replace(/\s/g, '_');
         }
         desc += '<a href="http://commons.wikimedia.org/wiki/File:' + showImage + '" target="_blank">';
-        desc += '<img src="https://commons.wikimedia.org/w/thumb.php?f=' + showImage + '&width=160" class="thumb" />';
+        desc += '<img src="https://commons.wikimedia.org/w/thumb.php?f=' + showImage + '&width=100" class="thumb" />';
         desc += '</a>';
 
         // info
@@ -242,8 +242,8 @@ function makePopup(feature) {
     else if (properties.descriptions.descr) {
         desc += capitalizeFirstLetter(properties.descriptions.descr);
     }
-    
-      if (properties.image || properties.descriptions.wikidata || properties.descriptions.descr) {
+
+    if (properties.image || properties.descriptions.wikidata || properties.descriptions.descr) {
         desc += '<br clear="both"/>';
     }
     desc += '</li></ul>';
